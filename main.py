565be8ad87c5550307dffc40bd0e554e2a574caa
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-08-18 02:52:13 +0000

    debug v22 (fixed syntax error)

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def on_command_error(ctx,error):
 @slash.slash(name="list_all_problem_ids", description= "List all problem ids")
 async def list_all_problem_ids(ctx):
   ctx.defer()
-  await ctx.send("\n".join([str(item) for item in mathProblems.keys())[:1930])
+  await ctx.send("\n".join([str(item) for item in mathProblems.keys()])[:1930])
 @slash.slash(name="generate_new_problems", description= "Generates new problems", options=[discord_slash.manage_commands.create_option(name="num_new_problems_to_generate", description="the number of problems that should be generated", option_type=4, required=True)])
 async def generate_new_problems(ctx, num_new_problems_to_generate):
   await ctx.defer()
