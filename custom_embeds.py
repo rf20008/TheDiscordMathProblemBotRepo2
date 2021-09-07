8c1eaa545ba42a99f4fd8f6a70adb9f8b5c943a9
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-09-07 15:04:17 +0000

    Fix a SyntaxError

--- custom_embeds.py
+++ custom_embeds.py
@@ -5,7 +5,7 @@ class SimpleEmbed(Embed):
     super().__init__(title=title,description=description,color=color)
     self.set_footer(footer)
 class ErrorEmbed(SimpleEmbed):
-  def __init__(self,description="",color=Color.red(),custom_title="Error",footer = Embed.Empty:
+  def __init__(self,description="",color=Color.red(),custom_title="Error",footer = Embed.Empty):
     super().__init__(title=custom_title,description=description, color=color)
     self.set_footer(footer)
 class SuccessEmbed(SimpleEmbed):
