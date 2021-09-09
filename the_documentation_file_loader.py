22e83583a90be717997e337431c10a896cc647f5
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-09-09 02:33:46 +0000

    Actually fix the TypeError

--- the_documentation_file_loader.py
+++ the_documentation_file_loader.py
@@ -44,7 +44,7 @@ No Mark: This is a command without user restrictions"""
   def get_documentation(self,documentationSource,documentationItem):
     _documentation = None
     documentation_from_json = self._load_documentation_file()
-    for item in [Item["file_name"] for Item in documentation_from_json.keys()]:
+    for item in [Item["file_name"] for Item in documentation_from_json]:
       if documentation_from_json[item]["file_name"] == documentationSource:
         _documentation = item
         break
