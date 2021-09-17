3cafda76c4f4d3ac7e06e408ecfca17588be4329
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-09-17 19:41:00 +0000

    Fix?

--- main.py
+++ main.py
@@ -88,9 +88,13 @@ async def on_slash_command_error(ctx, error):
         await ctx.reply(embed=ErrorEmbed("You are not the owner of this bot."))
         return
     #Embed = ErrorEmbed(custom_title="⚠ Oh no! Error: " + str(type(error)), description=("Command raised an exception:" + str(error)))
-    e = str(type(error))[8:]
+    e = (traceback.format_exception(etype=type(error),value=error,tb=error.__traceback__))
+    f = ""
+    for item in e:
+      f += item
+      f += "\n"
     
-    await ctx.reply(embed=ErrorEmbed("''"+(traceback.format_exception(etype=type(error),value=str(error),tb=error.__traceback__)) + "''",custom_title="Oh, no! An exception occurred"))
+    await ctx.reply(embed=ErrorEmbed("''" + f+ "''",custom_title="Oh, no! An exception occurred"))
 
     
 
