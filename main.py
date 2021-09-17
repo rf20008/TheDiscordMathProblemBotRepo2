afb6d26f612a4c05c228b6852a4e2064f3a41251
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-09-17 02:34:35 +0000

    Fix: /add_problem

--- main.py
+++ main.py
@@ -353,7 +353,7 @@ async def new_problem(ctx, answer, question, guild_question=False):
           guild_id=guild_id
         )
         print(problem)
-        main_cache.add_problem(problem.id, problem.guild_id,problem)
+        main_cache.add_problem(problem.guild_id, problem_id,problem)
         
 
         await ctx.reply(embed=SuccessEmbed("You have successfully made a math problem!",successTitle="Successfully made a new math problem."), ephemeral = True)
@@ -362,9 +362,12 @@ async def new_problem(ctx, answer, question, guild_question=False):
         problem_id = generate_new_id()
         if problem_id not in [problem.id for problem in main_cache.get_global_problems()]:
             break
+    print(problem_id)
+    
 
     problem = problems_module.MathProblem(question=question,answer=answer,id=problem_id,guild_id="null",author=ctx.author.id)
-    main_cache.add_problem(problem.id, problem.guild_id,problem)
+    print(problem.convert_to_dict())
+    main_cache.add_problem(problem_id, problem.guild_id,problem)
     await ctx.reply(embed=SuccessEmbed("You have successfully made a math problem!"), ephemeral = True)
 
 @slash.slash_command(name="check_answer", description = "Check if you are right", options=[Option(name="problem_id", description="the id of the problem you are trying to check the answer of", type=OptionType.INTEGER, required=True),Option(name="answer", description="your answer", type=OptionType.STRING, required=True),Option(name="checking_guild_problem", description="whether checking a guild problem", type=OptionType.BOOLEAN, required = False)])
