ff13a22428526f1583e9165fc0844da4cd98e824
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-09-18 22:38:33 +0000

    Fix: Multiple NameErrors + typeError

--- _error_logging.py
+++ _error_logging.py
@@ -6,10 +6,10 @@ def log_error(error,file_path):
         raise TypeError("error is not an error")
     e=traceback.format_exception(type(error),error,tb=error.__traceback__)
     try:
-        with open(file, "a") as f:
+        with open(file_path, "a") as f:
             f.write(time.asctime())
             f.write("\n" * 2)
-            f.write(e)
+            f.write("".join([str(item) for item in e]))
             f.write("\n" * 2)
     except Exception as exc:
         raise Exception("***File path not found.... or maybe something else happened.... anyway please report this :)***") from exc
