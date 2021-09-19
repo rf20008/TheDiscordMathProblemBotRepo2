d647827276dad41413ed53bfd11a81aa172100a2
BuildTools <64931063+rf20008@users.noreply.github.com>
2021-09-19 12:07:05 -0400

    Yes

--- return_intents.py
+++ return_intents.py
@@ -1,19 +1,5 @@
 from nextcord import Intents
 def return_intents():
-  i = Intents.default()
-  #i.bans = False
-  #i.dm_reactions=False
-  #i.dm_typing = False
-  #i.guild_reactions = False
-  #i.guild_typing = False
-  #i.guilds=True
-  #i.members=False
-  #i.integrations=False
-  #i.webhooks=False
-  #i.invites=False
-  #i.presences=False
-  #i.reactions=False
-  #i.typing=False
-  #i.voice_states=False
-  return i
+    i = Intents.default()
+    return i
   
