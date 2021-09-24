d3d647e957f038aebb0fa2858bb4bb2184253165
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-09-24 22:18:34 +0000

    uh

--- main.py
+++ main.py
@@ -822,7 +822,7 @@ async def debug(ctx,raw=False,send_ephermally=True):
         text = ""
         for item in debug_dict:
             if not isinstance([item], dict):
-                text += f"{debug_dict.get(item)}: " + f"''{item}''"
+                text += f"{debug_dict.get(item)}: " + f"''{item}''\n"
             else:
                 for item2 in item:
                     if not isinstance(item2, dict):
