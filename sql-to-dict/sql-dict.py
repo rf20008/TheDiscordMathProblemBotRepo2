34101bf4882a357c88c99e8ae3df28f96c1ec489
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-10-03 16:53:08 -0400

    fixed bug? also commit_and_commit

--- sql-to-dict/sql-dict.py
+++ sql-to-dict/sql-dict.py
@@ -24,7 +24,7 @@ class InvalidKeywordException(Exception):
 
 class SQLDict:
     "A custom class that represents a dictionary, but instead of being stored in RAM, it's stored in SQL"
-    def __init__(self,database_name, name,tablename, choosers = [SQLColumn("_key")], selects = [SQLColumn("_value")]):
+    def __init__(self,database_name, name,tablename, choosers = [SQLColumn("_key")], selects = [SQLColumn("_value")], init_new_table = True):
         self.database_name = database_name
         assert isinstance(choosers, list)
         assert isinstance(selects, list)
@@ -33,16 +33,19 @@ class SQLDict:
         self.name = name #each SqlDict
         #test connection to database, and initialize my table
         connection = self.connection
-        query = f"CREATE TABLE {self.name} (" 
-        for item in range(len(choosers)):
-            query += str(choosers[item])
-            if item != len(choosers) - 1:
-                query += ","
-            query += "\n"
-        query += ");"
+        if init_new_table:
+            query = f"CREATE TABLE {self.name} (" 
+            for item in range(len(choosers)):
+                query += str(choosers[item])
+                if item != len(choosers) - 1:
+                    query += ","
+                query += "\n"
+            query += ");"
 
-        connection.execute(query) #initialize the table
-
+            connection.execute(query) #initialize the table
+            connection.commit()
+        else:
+            connection.close()
     def __where_clause__(self,choosers):
         "A helper function that provides the help clause"
         query = "WHERE"
@@ -59,6 +62,25 @@ class SQLDict:
             return sqlite3.connect(self.database_name)
         except Exception as e:
             raise CannotConnectException(f"Can't connect to database {self.database_name}, maybe you made a typo?") from e
+    def _connect_and_commit(self,query,_execute_before_querying_=lambda: 0, 
+    _execute_between_querying_and_committing_=lambda: 0, 
+    _execute_after_committing_=lambda: 0,return_results_of_execution=False):
+        """A helper function for connecting and committing. You can pass in callables to _execute_before_querying, _execute_between_querying_and_commiting, and _execute_after_committing.
+    They must take a argument that is the instance of this sqldict. (and _execute_between_querying_and_committing and _execute_after_committing_ must take a Cursor object as its second argument) And they must be in the format (callable, options)."""
+        assert isinstance(_execute_after_committing_, function)
+
+        connection = self.connection
+        a=_execute_before_querying_[0](self,*_execute_before_querying_[1]) 
+        cursor = self.connection.cursor
+        self.connection.execute(query)
+        b = _execute_between_querying_and_committing_[0](self,cursor,*_execute_between_querying_and_committing_[1])
+
+        c=_execute_after_committing_[0](self,cursor,*_execute_after_committing_[1])
+        if return_results_of_execution:
+            return (a,b,c)
+        return None
+    
+        
     def get_item(self,choosers: list, wanted: list ):
         """Get the item with the exact choosers provided. Like dict[key]
         choosers is a list of SQLChoosers"""
@@ -79,7 +101,9 @@ class SQLDict:
             if item != len(wanted) - 1:
                 query += ", "
         query += self.__where_clause__(choosers)
-        self.connection.execute(query + f" {query} FROM {self.name}")
+        cursor = self.connection.cursor
+        e = cursor.execute(query + f" {query} FROM {self.name}")
+        
         self.connection.commit()
     def set_item(self,choosers,**selects):
         "Set the item located at choosers to selects (keyword arguments (keyword name: new value))"
@@ -95,8 +119,7 @@ class SQLDict:
             q += f"{item} = {selects[item]}"
         q += "\n"
         q += self.__where_clause__(choosers)
-        self.connection.execute(q)
-        self.connection.commit()
+        self._connect_and_commit(q)
     
 
         
