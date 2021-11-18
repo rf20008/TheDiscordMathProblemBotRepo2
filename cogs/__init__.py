00aaa43b3853b55eec16ba756f84070b85b60cd9
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-11-18 23:25:06 +0000

    Fix: __init__.py

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -5,3 +5,4 @@ from .developer_commands import DeveloperCommands
 from .helper_cog import HelperCog
 from .problems_cog import ProblemsCog
 from .quiz_cog import QuizCog
+from .misc_commands_cog import MiscCommandsCog
