d5a08beea371850665a0c9e77ff80f06809bf3e4
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-11-24 02:53:41 +0000

    Fix: cannot send an empty message

--- cogs/misc_commands_cog.py
+++ cogs/misc_commands_cog.py
@@ -73,8 +73,10 @@ class MiscCommandsCog(HelperCog):
         await inter.reply(type=5)  # Defer
         # We might not be able to respond in time because of the 100ms delay between user fetching
         # This is to respect the API rate limit.
-
+        if len(self.bot.trusted_users) == 0:
+            raise Exception("There are no trusted users!")
         __trusted_users = ""
+
         for user_id in self.bot.trusted_users:
             user = await self.bot.fetch_user(user_id=user_id)
             __trusted_users += f"""{user.name}#{user.discriminator}
