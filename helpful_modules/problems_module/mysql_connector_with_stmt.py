c620890ae11e3e7156b283249b583b3898e8fc5c
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-11-29 21:53:56 -0500

    Add a docstring

--- helpful_modules/problems_module/mysql_connector_with_stmt.py
+++ helpful_modules/problems_module/mysql_connector_with_stmt.py
@@ -3,8 +3,21 @@ import contextlib
 
 
 @contextlib.contextmanager
-def mysql_connection(*args, **kwargs):
+def mysql_connection(*args, **kwargs) -> None:
+    """A custom with statement to connect to a MySQL database.
+    You must take care to provide the correct arguments and keyword arguments, which will be directly passed to the mysql.connector.connect() method.
+    If an exception happens in the with statement, the connection will commit and close and then the exception will be raised.
+    Otherwise, the connection will commit and close. It will not return anything. :-)
+    This function is licensed under GPLv3."""
     connection = mysql.connector.connect(*args, **kwargs)
-    yield connection
-    connection.commit()
-    connection.close()
+    an_exception_occured = False # Did an exception occur?
+    try:
+        yield connection
+    except:
+        print("An exception happened in the bot. After closing resources, the exception will be raised")
+        connection.commit()
+        connection.close()
+        raise
+    finally:
+        connection.commit()
+        connection.close()
