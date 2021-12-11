aad00534f0a5ddb173eb45aa585691cb2fad8b87
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2021-12-11 13:44:42 -0500

    :-)

--- helpful_modules/custom_bot.py
+++ helpful_modules/custom_bot.py
@@ -1,3 +1,12 @@
-##import nextcord, dislash
-##class CustomBot:
-##    def __init__(self, )
+import nextcord, dislash
+class TheDiscordMathProblemBot(nextcord.ext.commands.Bot):
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tasks = kwargs.pop('tasks')
+        assert isinstance(self.tasks, dict)
+        for task in self.tasks.values():
+            assert isinstance(task, nextcord.ext.tasks.Loop)
+            task.start()
+    def get_task(self,task_name):
+        return self.tasks[task_name]
+        
