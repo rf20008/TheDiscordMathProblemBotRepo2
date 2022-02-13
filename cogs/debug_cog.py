4fc2fb3d2b14112d7032273cdf1e8e11088eedc5
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2022-02-13 14:59:10 -0500

    Added /eval2 which uses a modal

--- cogs/debug_cog.py
+++ cogs/debug_cog.py
@@ -14,7 +14,7 @@ from helpful_modules.custom_embeds import ErrorEmbed, SimpleEmbed, SuccessEmbed
 from helpful_modules.threads_or_useful_funcs import get_log
 
 from .helper_cog import HelperCog
-
+from os import urandom
 log = get_log(__name__)
 
 
@@ -23,6 +23,66 @@ class DebugCog(HelperCog):
 
     def __init__(self, bot: TheDiscordMathProblemBot):
         super().__init__(bot)
+
+    async def eval_code(self, inter, code: str):
+        thing_to_run = """async def func(): 
+                """  # maybe: just exec() directly
+        thing_to_run += textwrap.indent(code_, "    ", predicate=lambda l: True)
+        compiled = False
+        new_globals = {
+            "bot": self.bot,
+            "cache": self.cache,
+            "self": self,
+            "inter": inter,
+            "author": inter.author,
+        }
+        new_globals.update(
+            globals()
+        )  # credit: https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L234
+        try:
+            exec(thing_to_run, new_globals, locals())
+            compiled = True
+        except BaseException as e:
+            compiled = False
+            new_stderr.write("".join(format_exception(e)))
+        if (
+                "func" not in globals().keys()
+                and "func" not in locals().keys()
+                and compiled is True
+        ):
+            raise RuntimeError("func is not defined")
+        err = None
+        if compiled:
+            try:
+                with contextlib.redirect_stdout(new_stdout):
+                    with contextlib.redirect_stderr(new_stderr):
+                        if "func" in globals().keys():
+                            print(
+                                await globals()["func"](), file=new_stdout
+                            )  # Get the 'func' from the global variables and call it
+                            log.info("/eval ran (found in globals)")
+                        elif "func" in locals().keys():
+
+                            print(await (locals()["func"]()), file=new_stdout)  # Get func() from locals and call it
+                            log.info("/eval ran (found in locals)")
+                        else:
+                            raise Exception(f"""fatal: func() not defined""")
+            except BaseException as e:
+                new_stderr.write("".join(format_exception(e)))
+                err = None
+        await inter.send(
+            embed=SuccessEmbed(
+                f"""The code was successfully executed!
+        stdout: ```{new_stdout.getvalue()} ```
+        stderr: ```{new_stderr.getvalue()} ```"""
+            )
+        )
+        new_stdout.close()
+        new_stderr.close()
+        if err is not None:
+            raise err
+        else:
+            return
 
     def cog_slash_command_check(self, inter: disnake.ApplicationCommandInteraction):
         """A check that makes sure only bot owners can use this cog!"""
@@ -138,56 +198,55 @@ class DebugCog(HelperCog):
                 "We must both have the administrator permission to /eval!"
             )
         code_ = "\n".join(code.split("\\n"))  # Split the code by `\n`
-        thing_to_run = """async def func(): 
-        """  # maybe: just exec() directly
-        thing_to_run += textwrap.indent(code_, "    ", predicate=lambda l: True)
-        compiled = False
-        new_globals = {
-            "bot": self.bot,
-            "cache": self.cache,
-            "self": self,
-            "inter": inter,
-            "author": inter.author,
-        }
-        new_globals.update(
-            globals()
-        )  # credit: https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/admin.py#L234
-        try:
-            exec(thing_to_run, new_globals, locals())
-            compiled = True
-        except BaseException as e:
-            compiled = False
-            new_stderr.write("".join(format_exception(e)))
-        if (
-            "func" not in globals().keys()
-            and "func" not in locals().keys()
-            and compiled is True
-        ):
-            raise RuntimeError("func is not defined")
-        if compiled:
-            try:
-                with contextlib.redirect_stdout(new_stdout):
-                    with contextlib.redirect_stderr(new_stderr):
-                        if "func" in globals().keys():
-                            print(
-                                await globals()["func"](), file=new_stdout
-                            )  # Get the 'func' from the global variables and call it
-                            log.info("/eval ran (found in globals)")
-                        elif "func" in locals().keys():
+        await self.eval_code(inter, code)
 
-                            print(await (locals()["func"]()), file=new_stdout)  # Get func() from locals and call it
-                            log.info("/eval ran (found in locals)")
-                        else:
-                            raise Exception(f"""fatal: func() not defined""")
-            except BaseException as e:
-                new_stderr.write("".join(format_exception(e)))
-        await inter.send(
-            embed=SuccessEmbed(
-                f"""The code was successfully executed!
-stdout: ```{new_stdout.getvalue()} ```
-stderr: ```{new_stderr.getvalue()} ```"""
+    @commands.is_owner()
+    @checks.has_privileges(trusted=True,)
+    @commands.slash_command(
+        name='eval2',
+        description = "Evaluate Python code (for owners only)- this uses a modal"
+    )
+    async def eval2(self, inter):
+        """/eval2
+
+        R"""
+        if not await self.bot.is_owner(inter.author):
+            raise commands.NotOwner("You must be the owner to use /eval2")
+
+        if not await self.bot.is_trusted(inter.author):
+            raise RuntimeError("You must be trusted to use /eval2")
+
+        the_custom_id = urandom(20).hex()
+        text_inputs = [
+            disnake.ui.TextInput(
+                label = "What code do you want to run?",
+                custom_id = the_custom_id,
+                style=disnake.TextInputStyle.paragraph,
+                required=True,
+                max_length=5000
+
             )
-        )
-        new_stdout.close()
-        new_stderr.close()
-        return
+        ]
+        code_to_run = ""
+        async def callback(s, modal_inter: disnake.ModalInteraction):
+            if modal_inter.author.id != inter.author.id:
+                raise RuntimeError
+            nonlocal code_to_run
+            code_to_run = modal_inter.text_inputs[the_custom_id]
+            await modal_inter.send("Thanks for providing the code to run :-)")
+
+
+
+        modal_custom_id = urandom(20).hex()
+        modal: MyModal = MyModal(timeout=180,title="What code do you want to run?", custom_id=modal_custom_id, callback=callback, inter=inter)
+        modal.append_component(text_inputs)
+        await inter.response.send_modal(modal)
+        modal_inter = await self.bot.wait_for('modal_submit', check=lambda modal_inter: modal_inter.custom_id == modal_custom_id)
+
+        await self.eval_code(inter, code_to_run)
+
+def setup(bot: TheDiscordMathProblemBot):
+    bot.add_cog(DebugCog(bot))
+
+def teardown(bot: TheDiscordMathProblemBot):
+    bot.remove_cog("DebugCog")
