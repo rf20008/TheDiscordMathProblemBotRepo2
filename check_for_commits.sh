e019acd04f85b6971e1c597fa054d12aba484da9
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2022-04-06 23:46:12 -0400

    yes'

---
+++ check_for_commits.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# src: https://stackoverflow.com/questions/6006759/git-bash-how-to-check-if-theres-a-new-commit-available
+# license: CC-BY-SA 5.0
+# origin/branch we are interested in
+origin="origin"
+branch="beta"
+# last commit hash
+commit=$(git log -n 1 --pretty=format:%H "$origin/$branch")
+
+# url of the remote repo
+url=$(git remote get-url "$origin")
+
+for line in "$(git ls-remote -h $url)"; do
+    fields=($(echo $line | tr -s ' ' ))
+    test "${fields[1]}" == "refs/heads/$branch" || continue
+    test "${fields[0]}" == "$commit" && echo "nothing new" \
+        || echo "new commit(s) availble"
+    exit
+done
+
+echo "no matching head found on remote"
