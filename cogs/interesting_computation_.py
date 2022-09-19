781819947b407af8712df2edc72c6045627981b7
Samuel Guo <64931063+rf20008@users.noreply.github.com>
2022-09-19 04:12:23 +0000

    :)

--- cogs/interesting_computation_.py
+++ cogs/interesting_computation_.py
@@ -35,7 +35,7 @@ class InterestingComputationCog(HelperCog):
                 if not (
                     isinstance(remainders[i], int)
                     and isinstance(moduli[i], int)
-                    and modli[i] > remainders[i] > 0
+                    and moduli[i] > remainders[i] > 0
                 ):
                     raise ValueError("The CRT prerequsites are not satisifed!")
             self.remainders = remainders
@@ -45,15 +45,15 @@ class InterestingComputationCog(HelperCog):
             """Compute the CRT
             Algorithm credits to my brother"""
             product = 1
-            for i in range(len(moduli)):
-                product *= moduli[i]
+            for i in range(len(self.moduli)):
+                product *= self.moduli[i]
             sum = 0
             for i in range(len(moduli)):
                 num = product // moduli[i]
                 # compute the multiplicative number of product/b_i mod b_i
-                return_val = threads_or_useful_funcs.extended_gcd(product, moduli[i])
+                return_val = threads_or_useful_funcs.extended_gcd(product, self.moduli[i])
                 mult_inv = return_val[0][1]
-                sum += num * mult_inv * remainders[i]
+                sum += num * mult_inv * self.remainders[i]
             return sum
 
     @checks.no_insanely_huge_numbers_check()
@@ -69,23 +69,23 @@ class InterestingComputationCog(HelperCog):
         numsA = nums
 
         if len(moduliA) >= 300 or len(numsA) >= 300:
-            return inter.send(embed=ErrorEmbed("Too many moduli or nums!"))
+            return await inter.send(embed=ErrorEmbed("Too many moduli or nums!"))
         try:
             moduli = [int(item) for item in moduliA.split()]
         except ValueError:
-            inter.send(
+            await inter.send(
                 embed=ErrorEmbed("Could not convert moduli to a list of numbers!")
             )
             raise
-        if len(moduli_) >= 100:
-            return inter.send(embed=ErrorEmbed("Too many moduli!"))
+        if len(moduli) >= 100:
+            return await inter.send(embed=ErrorEmbed("Too many moduli!"))
         for i in moduli:
             if i <= 0 or i > 10**30:
                 return inter.send("All the remainders must be positive")
         for i in range(len(moduli) - 1):
-            for j in range(i, len(moduli)):
-                if math.gcd(moduli[i], moduli[j]) != 1:
-                    return inter.send(
+            for j in range(i+1, len(moduli)):
+                if gcd(moduli[i], moduli[j]) != 1:
+                    return await inter.send(
                         embed=ErrorEmbed(
                             "The chinese remainder theorem doesn't hold unless the numbers are relatively prime"
                         )
@@ -93,8 +93,8 @@ class InterestingComputationCog(HelperCog):
             if moduli[i] >= MAX_NUM:
                 raise MathProblemsModuleException("Remainder too big")
             for j in range(i, len(moduli)):
-                if math.gcd(moduli[i], moduli[j]) != 1:
-                    return inter.send(
+                if gcd(moduli[i], moduli[j]) != 1:
+                    return await inter.send(
                         embed=ErrorEmbed(
                             "The chinese remainder theorem doesn't hold unless the numbers are relatively prime"
                         )
@@ -103,18 +103,18 @@ class InterestingComputationCog(HelperCog):
         try:
             nums = [int(item) for item in numsA.split()]
         except ValueError:
-            inter.send(embed=ErrorEmbed("Could not convert nums to a list of numbers!"))
+            await inter.send(embed=ErrorEmbed("Could not convert nums to a list of numbers!"))
             raise
         if len(nums) >= 100:
-            return inter.send(embed=ErrorEmbed("Too many nums!"))
+            return await inter.send(embed=ErrorEmbed("Too many nums!"))
         if len(moduli) != len(nums):
-            return inter.send(
+            return await inter.send(
                 embed=ErrorEmbed("#moduli != # nums : therefore we can't use CRT.")
             )
 
         for i in range(len(nums)):
             if nums[i] <= 0:
-                return inter.send("All the remainders must be positive!")
+                return await inter.send("All the remainders must be positive!")
             if nums[i] >= MAX_NUM:
                 raise MathProblemsModuleException("Number too big")
             if nums[i] >= moduli[i]:
